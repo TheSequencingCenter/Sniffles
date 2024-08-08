a2a45061e778799690e542cad8b2f8b4e435678e
The Sequencing Center <info@thesequencingcenter.com>
2024-08-08 11:10:55 -0600

    add error check for existance of S3 bucket

--- sniffles.py
+++ sniffles.py
@@ -9,6 +9,12 @@ import os
 import subprocess
 import sys
 
+# Third-party imports
+import boto3
+from   botocore.exceptions import ClientError
+from   botocore.exceptions import NoCredentialsError
+from   botocore.exceptions import PartialCredentialsError
+
 # Local application/library specific imports
 from utilities.fileUtil   import clear_files
 from utilities.loggerUtil import logger
@@ -75,11 +81,29 @@ def clear_all_files() -> None:
     logger.info("Clear files...")
     clear_files()
 
+def check_s3_bucket_exists(bucket_name: str) -> bool:
+    """Check if the specified S3 bucket exists."""
+    s3 = boto3.client('s3')
+    try:
+        s3.head_bucket(Bucket=bucket_name)
+        return True
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        else:
+            raise
+    except NoCredentialsError:
+        logger.error("ERROR: AWS credentials not found.")
+        sys.exit(1)
+    except PartialCredentialsError:
+        logger.error("ERROR: Incomplete AWS credentials.")
+        sys.exit(1)
+
 def copy_sample_files_S3_to_EC2(bucket_name: str, sample_name: str) -> None:
     """Copy sample files from S3 to EC2.
     
      Parameters:
-       --recursive : Copy files recursively.
+       --recursive : Copy files recursively.  This is a required parameter.
     """
     command = (
         # f"aws s3 cp s3://{bucket_name}/{sample_name}/ "
@@ -194,6 +218,22 @@ if __name__ == "__main__":
             set_environment_variables(args.samplename)
         except Exception as e:
             logger.error(f"ERROR: Could not set environment variables {e}")
+            sys.exit(1)
+
+        # Check if S3 bucket exists
+        try:
+            if not check_s3_bucket_exists(args.bucketname):
+                logger.error(f"ERROR: AWS S3 bucket '{args.bucketname}' does not exist.")
+                sys.exit(1)
+        except NoCredentialsError:
+            logger.error("ERROR: AWS credentials not found.")
+            sys.exit(1)
+        except PartialCredentialsError:
+            logger.error("ERROR: Incomplete AWS credentials.")
+            sys.exit(1)
+        except Exception as e:
+            logger.error(f"ERROR: An error occurred while checking the S3 bucket: {e}")
+            sys.exit(1)
 
         # delete all files in subdirectories to set initial state for application
         try:
@@ -201,14 +241,15 @@ if __name__ == "__main__":
             clear_files()
         except Exception as e:
             logger.error(f"ERROR: Failed to clear files: {e}")
+            sys.exit(1)
 
         # 1. copy sample files from S3 seqcenter-samples bucket to EC2 POD5 directory
         try:
             logger.info("Copy sample POD5 files from S3 to EC2...")
             copy_sample_files_S3_to_EC2(args.bucketname, args.samplename)
-            sys.exit(0)
         except Exception as e:
             logger.error(f"ERROR: Failed to copy sample POD5 files from S3 to EC2: {e}")
+            sys.exit(1)
 
         # convert fast5 file to pod5 file
         # try:
@@ -216,6 +257,7 @@ if __name__ == "__main__":
         #     convert_fast5_to_pod5()
         # except Exception as e:
         #     logger.error(f"ERROR: Failed to convert fast5 to pod5: {e}")
+        #     sys.exit(1)
 
         # 2. convert pod5 file to bam file
         try:
@@ -223,6 +265,7 @@ if __name__ == "__main__":
             convert_pod5_to_bam()
         except Exception as e:
             logger.error(f"ERROR: Failed to convert pod5 to bam: {e}")
+            sys.exit(1)
 
         # 3. sort bam file
         try:
@@ -230,6 +273,7 @@ if __name__ == "__main__":
             sort_bam_file()
         except Exception as e:
             logger.error(f"ERROR: Failed to sort bam file: {e}")
+            sys.exit(1)
 
         # 4. create sorted bam index file
         try:
@@ -237,6 +281,7 @@ if __name__ == "__main__":
             create_sorted_bam_index_file()
         except Exception as e:
             logger.error(f"ERROR: Failed to create sorted bam index file: {e}")
+            sys.exit(1)
 
         # 5. perform structural variant calling
         try:
@@ -244,6 +289,7 @@ if __name__ == "__main__":
             run_sniffles()
         except Exception as e:
             logger.error(f"ERROR: Failed to perform structural variant calling: {e}")
+            sys.exit(1)
 
         logger.info("Finish Sniffles...")
 
